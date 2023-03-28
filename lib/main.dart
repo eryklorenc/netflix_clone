c92fffe3c91c34b991a6b3174beed87ead1e740a
Eryk Lorenc <reus2000@wp.pl>
2023-03-28 22:15:57 +0200

    Install Auth Gate

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:netflix_clone/features/auth/auth_gate.dart';
 import 'package:netflix_clone/firebase_options.dart';
 
 void main() async {
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: const AuthGate(),
     );
   }
 }
